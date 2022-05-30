78d3e4ae87210ee14159cb27d9b466ffed002653
RotemAs <91944578+RotemAs@users.noreply.github.com>
2022-05-30 16:20:04 +0300

    2.3

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-
+// globals
 let game = "";
 const userStartButton = document.getElementById("btn__reset");
 const projectedKeyboard = document.getElementById("qwerty");
 const keys = document.querySelectorAll(".key");
 
+// step 4  event listener to the "Start Game" button 
 userStartButton.addEventListener("click", () => {
     game = new Game();
     game.startGame();
 });
+
+// step 4 event listeners to each of the onscreen keyboard buttons
 
 projectedKeyboard.onclick = (e) => {
     let target = e.target;
@@ -18,6 +21,8 @@ projectedKeyboard.onclick = (e) => {
         game.handleInteraction(target);
     };
 };
+
+//event listeners to each of the physical computer keyboard key 
 
 document.addEventListener("keyup", (e) => {
     for (let i=0; i < keys.length; i++) {
